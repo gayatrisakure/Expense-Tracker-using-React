135a7175783ef151e820a86af270b18fecca8c43
Gayatri Sakure <gayatrisakure@gmail.com>
2023-02-17 20:12:46 +0530

    Updates

---
+++ precache-manifest.2662f59beaac4450fd5884c37d477c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca074a548ad27d51284b69d0a9e52011",
+    "url": "/Expense-Tracker-using-React/index.html"
+  },
+  {
+    "revision": "5d68aae6102833811500",
+    "url": "/Expense-Tracker-using-React/static/css/main.db29d858.chunk.css"
+  },
+  {
+    "revision": "c62c83a27cbb7ede3f13",
+    "url": "/Expense-Tracker-using-React/static/js/2.278df7d2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Expense-Tracker-using-React/static/js/2.278df7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d68aae6102833811500",
+    "url": "/Expense-Tracker-using-React/static/js/main.591727a3.chunk.js"
+  },
+  {
+    "revision": "b264bb2985f0bc83be6e",
+    "url": "/Expense-Tracker-using-React/static/js/runtime-main.92d9fc97.js"
+  }
+]);
